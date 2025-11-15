ba073c74f8045a3cc7f1642e0036b7dd47a1a72e
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-11-15 00:35:09 +0000

    build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+flutter pub get
+flutter build ios --build-number "$BUILD_NUMBER" --config-only --no-codesign \
+          --dart-define GOOGLE_MAPS_CLIENT_ID="$GOOGLE_MAPS_CLIENT_ID" \
+          --dart-define GOOGLE_MAPS_SIGNING_SECRET="$GOOGLE_MAPS_SIGNING_SECRET"
