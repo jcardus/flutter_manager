f796b8968a731130fbb5a9c8dd5a4864504ea496
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-11-18 15:20:25 +0000

    TRACCAR_BASE_URL

--- build.sh
+++ build.sh
@@ -14,4 +14,5 @@ fi
 flutter pub get
 flutter build ios --build-number "$BUILD_NUMBER" --config-only --no-codesign \
           --dart-define GOOGLE_MAPS_CLIENT_ID="$GOOGLE_MAPS_CLIENT_ID" \
-          --dart-define GOOGLE_MAPS_SIGNING_SECRET="$GOOGLE_MAPS_SIGNING_SECRET"
+          --dart-define GOOGLE_MAPS_SIGNING_SECRET="$GOOGLE_MAPS_SIGNING_SECRET" \
+          --dart-define TRACCAR_BASE_URL="$TRACCAR_BASE_URL"
