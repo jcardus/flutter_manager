c76ac186e437625eec177bc3cb0e07dec53d890f
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-11-18 23:59:45 +0000

    TRACCAR_BASE_URL

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ flutter build $1 \
           --build-number "$BUILD_NUMBER" \
           --dart-define GOOGLE_MAPS_CLIENT_ID="$GOOGLE_MAPS_CLIENT_ID" \
           --dart-define GOOGLE_MAPS_SIGNING_SECRET="$GOOGLE_MAPS_SIGNING_SECRET" \
-          --dart-define TRACCAR_BASE_URL="$TRACCAR_BASE_URL"
+          --dart-define TRACCAR_BASE_URL="https://traccar-eu.fleetmap.pt"
