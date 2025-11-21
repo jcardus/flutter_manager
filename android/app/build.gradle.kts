3607863f375e6d58a3495e022eb86dc106e2bf42
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-11-21 01:37:15 +0000

    moviflote

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fleetmap.fleetmanager"
+    namespace = "com.fleetmap.moviflotte"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.1.13356709"
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.fleetmap.fleetmanager"
+        applicationId = "com.fleetmap.moviflotte"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
