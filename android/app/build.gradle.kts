82fe6762d7730eaca6598999f142b65edbbab1e8
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-11-22 13:21:47 +0000

    fix android 16

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.fleetmap.fleetmanager"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36  // Explicitly set for Android 16 compatibility
     ndkVersion = "28.1.13356709"
 
     compileOptions {
@@ -33,8 +33,8 @@ android {
         applicationId = "com.fleetmap.fleetmanager"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = flutter.minSdkVersion  // Android 5.0+
+        targetSdk = 36  // Android 16
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
