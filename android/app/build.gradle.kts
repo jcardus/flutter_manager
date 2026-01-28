2f4965678c2c374387c2128ae47a4227cdf2e421
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2026-01-28 23:34:17 +0000

    com.nogartel.manager

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fleetmap.fleetmanager"
+    namespace = "com.nogartel.manager"
     compileSdk = 36  // Explicitly set for Android 16 compatibility
     ndkVersion = "28.1.13356709"
 
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.fleetmap.fleetmanager"
+        applicationId = "com.nogartel.manager"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion  // Android 5.0+
