4b787f5bd2efdd258d2f95f18a95f0e871faa275
jcardeira <joaquim.cardeira@gmail.com>
2026-04-11 22:40:55 +0100

    flutter_map macos

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,13 +6,11 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
-brew install librsvg
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
-scripts/generate_icons.sh
 ./build.sh "ios --config-only --no-codesign"
 exit 0
